06a2b6bfd9e3b7580addfce3fe8d665601a16bc5
senthilrajendran1 <50818680+senthilrajendran1@users.noreply.github.com>
2019-05-20 10:07:22 +0100

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup (
+    name='gamesys_test' ,
+    version='' ,
+    packages=['gamesys' , 'gamesys.resources'] ,
+    url='' ,
+    license='' ,
+    author='SRA39' ,
+    author_email='' ,
+    description=''
+)
